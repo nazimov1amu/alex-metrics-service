1ae71f38c24fc2bfecd91314be1a3947a494a708
Alexander <nazimov.amu@yandex.ru>
2026-07-20 12:50:58 +0300

    Refactor metrics handling and add bulk retrieval functionality
    - Removed obsolete metrics tests to streamline the codebase.
    - Enhanced metrics handler to support bulk retrieval of metrics with a new GetBulk method.
    - Updated the Update and Get methods to handle errors more effectively.
    - Introduced HTML templates for rendering metrics in a user-friendly format.
    - Refactored service and repository layers to accommodate new functionality.

--- internal/handler/metrics.go
+++ internal/handler/metrics.go
@@ -1,14 +1,23 @@
 package handler
 
 import (
-	"encoding/json"
-	"log"
+	"embed"
+	"errors"
+	"fmt"
+	"html/template"
 	"net/http"
+	"strconv"
 
 	"github.com/Alexunder2003/alex-metrics-service/internal/model"
 	"github.com/Alexunder2003/alex-metrics-service/internal/service"
 	"github.com/go-chi/chi/v5"
 )
+
+//go:embed templates/*.html
+var templatesFS embed.FS
+
+var metricsTmpl = template.Must(template.ParseFS(templatesFS, "templates/*.html"))
+
 
 type Handler struct {
 	svc *service.MetricsService
@@ -19,34 +28,74 @@ func New(svc *service.MetricsService) *Handler {
 }
 
 func (h *Handler) Update(w http.ResponseWriter, r *http.Request) {
-	log.Println("Update request received")
 	input := model.MetricsInput{
 		Name:     chi.URLParam(r, "name"),
 		MType:    chi.URLParam(r, "type"),
 		RawValue: chi.URLParam(r, "value"),
 	}
 
-	if err := h.svc.Update(input); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	metric, err := h.svc.Update(input)
+	if err != nil {
+		switch {
+		case errors.Is(err, service.ErrInvalidMetricType):
+			http.Error(w, err.Error(), http.StatusNotFound)
+		case errors.Is(err, service.ErrInvalidCounterValue):
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		case errors.Is(err, service.ErrInvalidGaugeValue):
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		default:
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 		return
 	}
+
 	w.WriteHeader(http.StatusOK)
+
+	switch metric.MType {
+	case model.Counter:
+		w.Write(fmt.Appendf(nil, "%d", *metric.Delta))
+	case model.Gauge:
+		w.Write(fmt.Appendf(nil, "%f", *metric.Value))
+	}
 }
+
 
 func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
 	name := chi.URLParam(r, "name")
 
 	metric, err := h.svc.Get(name)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
+		switch {
+		case errors.Is(err, service.ErrMetricNotFound):
+			http.Error(w, err.Error(), http.StatusNotFound)
+		default:
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	response, err := json.Marshal(metric)
+	switch metric.MType {
+	case model.Counter:
+		w.Write([]byte(strconv.FormatInt(*metric.Delta, 10)))
+	case model.Gauge:
+		w.Write([]byte(strconv.FormatFloat(*metric.Value, 'f', -1, 64)))
+	}
+}
+
+func (h *Handler) GetBulk(w http.ResponseWriter, r *http.Request)  {
+	metrics, err := h.svc.GetBulk()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Write(response)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	err = metricsTmpl.Execute(w, metrics)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 }
