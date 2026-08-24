9663532ccbc8a4130aa7655dd7f237ed250f48f4
Alexander <nazimov.amu@yandex.ru>
2026-08-24 13:17:45 +0300

    Remove obsolete CompressorResponseWriter and related middleware from encoding package; delete unused README file from repository. Streamline codebase by eliminating unnecessary components.

--- internal/encoding/encoding.go
+++ internal/encoding/encoding.go
@@ -5,7 +5,6 @@ import (
 	"compress/gzip"
 	"io"
 	"net/http"
-	"strings"
 )
 
 type CompressorResponseWriter struct {
@@ -33,56 +32,3 @@ func Decompress(data io.ReadCloser) (io.ReadCloser, error) {
 	return gz, nil
 }
 
-func NewCompressorResponseWriter(w http.ResponseWriter) *CompressorResponseWriter {
-	return &CompressorResponseWriter{
-		rw:     w,
-		grw:    gzip.NewWriter(w),
-	}
-}
-
-
-func (w *CompressorResponseWriter) Write(data []byte) (int, error) {
-	compressed, err := Compress(data)
-	if err != nil {
-		return 0, err
-	}
-
-	return w.rw.Write(compressed)
-}
-
-func (w *CompressorResponseWriter) Header() http.Header {
-	return w.rw.Header()
-}
-
-func (w *CompressorResponseWriter) WriteHeader(statusCode int) {
-	w.rw.WriteHeader(statusCode)
-}
-
-func WithCompressing(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
-			defer r.Body.Close()
-			
-			decompressed, err := Decompress(r.Body)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			r.Body = decompressed
-		}
-
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		crw := NewCompressorResponseWriter(w)
-		crw.Header().Set("Content-Encoding", "gzip")
-		next.ServeHTTP(crw, r)
-	})
-}
-
-func CompressingMiddleware(next http.Handler) http.Handler {
-	return WithCompressing(next)
-}
